51df977d6eb66f9165d15c337c0728ec6852d33b
Viktor Kanaev <vikkanaev@gmail.com>
2022-03-18 11:45:38 +0400

    task2 refactoring

--- task2.rb
+++ task2.rb
@@ -1,12 +1,36 @@
 class Search
   def search(type)
     case type
-  	when :job
-  	  search_jobs
-  	when :user
-  	  search_users
-  	else
-  	  raise 'Unknown search type'
+  	when :job then JobSearch.new.search
+  	when :user then JobSearch.new.search
+  	else BaseSearch.new.search
   	end
   end
 end
+
+class BaseSearch
+  def search
+    raise NotImplementedError
+  end
+end
+
+class JobSearch < Search
+  def search
+    :search_jobs
+  end
+end
+
+class UserSearch < Search
+  def search
+    :search_users
+  end
+end
+
+# Search.new.search(:job)
+#  => :search_jobs
+
+# Search.new.search(:user)
+#  => :search_jobs
+
+# Search.new.search(:food)
+# => NotImplementedError (NotImplementedError)
